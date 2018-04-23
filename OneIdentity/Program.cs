4f3944a940d8b4cbd57a840ac099597c2111f65a
Toby Austin <toby@apiaster.co.uk>
2018-04-23 22:59:33 +0100

    Add project files.

---
+++ OneIdentity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OneIdentity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
